ecd7a7180182f1435d09e9ad4bc1b7ad5eb645d0
bjoh3944 <ben.johnston@sydney.edu.au>
2017-09-14 15:03:03 +1000

    Adding indoor and outdoor 300W results

---
+++ experiments/Sec4_ModelDefinition/300W_aam_patch_indoor.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# S.D.G
+
+"""
+300W dataset holistic AAM
+
+
+
+
+
+:author: Ben Johnston
+:license: 3-Clause BSD
+
+"""
+
+# Imports
+from aam import AAM
+from menpofit.aam import PatchAAM
+
+model = AAM('/home/bjoh3944/predPap-ben/datasets/ibug/300W/01_Indoor',
+            model_type=PatchAAM,
+            basename='300W_aam_patch_indoor')
+model.load_data()
+model.train_model(batch_size=None)
+model.fit_model()
+model.predict_test_set()
+model.generate_cdf()
